71c95515c1ff985306ca5f5d4728d8084f6e2e23
igor gazivoda <igor.gazivoda@jobcloud.ch>
2019-08-15 22:56:15 +0200

    multiple servers and accounts logic

--- index.js
+++ index.js
@@ -5,43 +5,6 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 
-let fromEmail = process.env.FROM_EMAIL;
-if (!fromEmail) {
-    console.log('Missing FROM_EMAIL env var');
-    process.exit(1);
-}
-
-const transporter = nodemailer.createTransport({
-    // service: process.env.SMTP_SERVICE,
-    tls: { rejectUnauthorized: false },
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT, 10) || 465,
-    secure: true, // secure:true for port 465, secure:false for port 587
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
-    }
-});
-
-let fromEmail2 = process.env.FROM_EMAIL2;
-if (!fromEmail2) {
-    console.log('Missing FROM_EMAIL2 env var');
-    // process.exit(1);
-}
-
-const transporter2 = nodemailer.createTransport({
-    // service: process.env.SMTP_SERVICE,
-    tls: { rejectUnauthorized: false },
-    host: process.env.SMTP_HOST2,
-    port: parseInt(process.env.SMTP_PORT2, 10) || 465,
-    secure: true, // secure:true for port 465, secure:false for port 587
-    auth: {
-        user: process.env.SMTP_USER2,
-        pass: process.env.SMTP_PASSWORD2
-    }
-});
-
-
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
@@ -80,37 +43,37 @@ app.post('/send', (req, res) => {
         return;
     }
     let replyTo = req.body.replyTo;
+    let fromEmail = req.body.replyTo;
+
     if (replyTo && (typeof replyTo !== 'string' || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(replyTo))) {
         res.status(400).send('Invalid replyTo');
         return;
     }
-    let emailServer = req.body.emailServer;
-
 
     fromName = fromName.substring(0, 100).replace(/[^a-zA-Z0-9\-\.\_\ ]/, '');
     subject = subject.substring(0, 200);
     body = body.substring(0, parseInt(process.env.MAX_BODY_SIZE, 10) || 10000);
 
-    const mailOptions = {
-        from: '"' + fromName + '" <' + fromEmail + '>',
-        to: toEmail,
-        subject: subject,
-        text: body,
-        html: html
-    };
-    if (replyTo) {
-        mailOptions.replyTo = replyTo;
-    }
-
-    // Default mail server .me
-    if (!emailServer || typeof emailServer !== 'string') {
-
+        if (!fromEmail) {
+            process.exit(1);
+        }
+        const transporter = nodemailer.createTransport({
+            tls: { rejectUnauthorized: false },
+            host: 'mail.domainlions.' + fromEmail.split('.')[1],
+            port: parseInt(process.env.SMTP_PORT, 10) || 465,
+            secure: true,
+            auth: {
+                user: fromEmail,
+                pass: process.env.SMTP_PASSWORD
+            }
+        });
         const mailOptions = {
             from: '"' + fromName + '" <' + fromEmail + '>',
             to: toEmail,
             subject: subject,
             text: body,
-            html: html
+            html: html,
+            replyTo: replyTo
         };
         transporter.sendMail(mailOptions, (err, info) => {
             if (err) {
@@ -118,30 +81,12 @@ app.post('/send', (req, res) => {
                 res.status(500).send(err.message);
                 return;
             }
-            console.log('Message %s sent from .me : %s', info.messageId, info.response);
+            console.log('Message sent from '
+                + fromEmail + ' to ' + toEmail +
+                '  :', info.messageId, info.response);
             res.send('sent');
         });
-    }
-    // Second server .xyz case 
-    else {
-
-        const mailOptions = {
-            from: '"' + fromName + '" <' + fromEmail2 + '>',
-            to: toEmail,
-            subject: subject,
-            text: body,
-            html: html
-        };
-        transporter2.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err.message);
-                return;
-            }
-            console.log('Message %s sent from .xyz : %s', info.messageId, info.response);
-            res.send('sent');
-        });
-    }
+    // }
 
 });
 
